308ddcbf160c29fc3e480c9e101ae69162446150
raunak <iamraunak299@gmail.com>
2022-12-24 12:40:56 +0530

    netlify.toml

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,66 @@
-
-import './App.css';
-import Home from  './Pages/Home/Home';
-import {Switch,Route,Redirect} from 'react-router-dom'
-import React, { useContext } from 'react';
-import Contest from './Pages/Contest/Contest';
-import Result from './Pages/Result/Result';
-import Authentication from './Pages/Authentication/Authentication';
-import Profile from './Pages/Profile/Profile';
-import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
-import Dashboard from './Pages/Dashboard/Dashboard';
-import { AuthContext } from './Store/AuthContext';
-
+import "./App.css";
+import Home from "./Pages/Home/Home";
+import { Switch, Route, Redirect } from "react-router-dom";
+import React, { useContext } from "react";
+import Contest from "./Pages/Contest/Contest";
+import Result from "./Pages/Result/Result";
+import Authentication from "./Pages/Authentication/Authentication";
+import Profile from "./Pages/Profile/Profile";
+import PrivateRoute from "./Components/PrivateRoute/PrivateRoute";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import { AuthContext } from "./Store/AuthContext";
 
 function App() {
-
   let authContx = useContext(AuthContext);
   let login = authContx.token;
 
   return (
     <React.Fragment>
       <Switch>
+        <Route path="/" exact>
+          {/* <PrivateRoute> */}
+          <Redirect to="/home" />
+          {/* </PrivateRoute> */}
+        </Route>
 
-      <Route path='/' exact>
-        <PrivateRoute>
-        <Redirect to='/home' />
-        </PrivateRoute>
-      </Route>
+        <Route path="/home" exact>
+          <Home />
+        </Route>
 
-      <Route path='/home' exact>
-        <Home />
-      </Route>
+        <Route path="/contest/:contestId" exact>
+          <PrivateRoute>
+            <Contest />
+          </PrivateRoute>
+        </Route>
 
-      <Route path='/contest/:contestId' exact>
-        <PrivateRoute>
-        <Contest />
-        </PrivateRoute>
-      </Route>
+        <Route path="/result/:contestId" exact>
+          <PrivateRoute>
+            <Result />
+          </PrivateRoute>
+        </Route>
 
-      
-      <Route path='/result/:contestId' exact>
-      <PrivateRoute>
-        <Result />
-        </PrivateRoute>
-      </Route>
+        <Route path="/authentication" exact>
+          {login && <Redirect to="/profile" />}
+          {!login && <Authentication />}
+        </Route>
 
-      <Route path='/authentication' exact>
-        {login && <Redirect to='/profile' />}
-        {!login && <Authentication/>  }
-      </Route>
+        <Route path="/profile">
+          <PrivateRoute>
+            <Profile />
+          </PrivateRoute>
+        </Route>
 
-      <Route path='/profile'>
-      <PrivateRoute>
-        <Profile />
-        </PrivateRoute>
-      </Route>
+        <Route path="/dashboard">
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        </Route>
 
-      
-      <Route path='/dashboard'>
-      <PrivateRoute>
-        <Dashboard />
-        </PrivateRoute>
-      </Route>
-  
-      <Route path='*'>
-          <Redirect to='/' />
-      </Route>
-
-      
-
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
       </Switch>
-      </React.Fragment>
+    </React.Fragment>
   );
 }
 
